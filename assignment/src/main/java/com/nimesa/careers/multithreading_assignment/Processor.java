4ec61585a2f087499fbf9df18b08982aef0c7cc9
Shashwat <nemashashwat@gmail.com>
2024-05-03 23:50:02 +0530

    updated Processor code to run pin parallel based on User, TaskType, Priority Criteria instead of running sequentially

--- assignment/src/main/java/com/nimesa/careers/multithreading_assignment/Processor.java
+++ assignment/src/main/java/com/nimesa/careers/multithreading_assignment/Processor.java
@@ -1,8 +1,11 @@
 package com.nimesa.careers.multithreading_assignment;
 
-import java.util.*;
-import java.util.concurrent.*;
-import java.util.function.Function;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Queue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.stream.Collectors;
 
 public class Processor {
 
@@ -21,18 +24,41 @@ public class Processor {
     }
 
     public List<TaskResponse> execute() throws InterruptedException {
-
-        /**
-         *Sequential execution
-         */
         List<TaskResponse> taskResponses = new ArrayList<>();
-        for (TaskRequest taskRequest : queue) {
-            System.out.println("Starting Task "+taskRequest.getId());
-            Task task = new Task(taskRequest);
-            TaskResponse response = task.run();
-            System.out.println("Completed Task "+response.getId() +"With Status"+response.getStatus());
-            taskResponses.add(response);
-        }
+        /* All Task in outer List will be executed in parallel in respect to each other and All Task within Inner List will be executed sequentially in respect to each other in inner list according to priority */
+        List<List<TaskRequest>> groupedTasks = new ArrayList<>();
+        groupTaskForParallelExecution(groupedTasks);
+        executeTaskInParallel(groupedTasks, taskResponses);
         return taskResponses;
     }
+
+    private void groupTaskForParallelExecution(List<List<TaskRequest>> groupedTasks) {
+        queue.stream().collect(Collectors.groupingBy(TaskRequest::getSubmittedBy))
+                        .values().forEach(sameUserTaskRequests -> {
+                            sameUserTaskRequests.stream().collect(Collectors.groupingBy(TaskRequest::getType))
+                                    .values().forEach(sameUserTaskTypeRequests -> {
+                                        sameUserTaskTypeRequests.sort(Comparator.comparing(TaskRequest::getPriority));
+                                        if(!sameUserTaskTypeRequests.isEmpty()){
+                                            groupedTasks.add(sameUserTaskTypeRequests);
+                                        }
+                                    });
+                });
+    }
+
+    private static void executeTaskInParallel(List<List<TaskRequest>> groupedTasks, List<TaskResponse> taskResponses) {
+        groupedTasks.parallelStream().forEach(taskRequestList -> {
+            taskRequestList.forEach(taskRequest -> {
+                Task task = new Task(taskRequest);
+                try {
+                    System.out.println("Starting Task "+taskRequest.getId());
+                    TaskResponse response = task.run();
+                    System.out.println("Completed Task "+response.getId() +" With Status "+response.getStatus());
+                    taskResponses.add(response);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            });
+        });
+    }
+
 }
